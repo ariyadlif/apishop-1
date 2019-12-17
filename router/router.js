c883fd29c1bb39523410a2ed26c7e25a753ffcc0
User achmadriyadlifachmi <ar.arf.rf@gmail.com>
2019-12-17 11:57:56 +0700

    apishop1

---
+++ router/router.js
@@ -0,0 +1,20 @@
+const productsController = require('../controller/productsController')
+const userController = require('../controller/userController')
+
+module.exports = app => {
+	app.get('/', productsController.home)
+
+	//api products
+	app.get('/products', productsController.listProduct)
+	app.get('/products/:id', productsController.detailProduct)
+	app.post('/products/', productsController.tambahProduct)
+	app.put('/products/:id', productsController.ubahProduct)
+	app.delete('/products/:id', productsController.hapusProduct)
+
+	//api user
+	app.get('/user', userController.listUser)
+	app.get('/user/:id', userController.detailUser)
+	app.post('/user/', userController.tambahUser)
+	app.put('/user/:id', userController.ubahUser)
+	app.delete('/user/:id', userController.hapusUser)
+}
